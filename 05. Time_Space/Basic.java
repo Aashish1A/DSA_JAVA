a62eccedec6f75888d26dbe1dce3fad82ef72ef7
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-02-22 00:13:38 +0530

    Learning about time and space complexity

--- 05. Time_Space/Basic.java
+++ 05. Time_Space/Basic.java
@@ -2,10 +2,21 @@ import java.util.Scanner;
 
 public class Basic{
     public static void main(String[] args) {
+
+        // WAP to find sum of given number and find the time complexity.
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number : ");
         int n = sc.nextInt();
 
+        //first approach
+        int sum = 0;
+        for(int i=1; i<=n; i++){
+            sum  = sum + i;
+        }
+        System.out.println("The sum of given no is : " + sum);
+
+        // second approach
         int ans = n*(n+1)/2;
         System.out.print("The sum of given no is : " + ans);
 

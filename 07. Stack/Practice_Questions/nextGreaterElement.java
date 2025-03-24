a30e81a029a161d77adacd2ec133db25a07970d6
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-03-24 18:38:00 +0530

    Solving problems related stack

---
+++ 07. Stack/Practice_Questions/nextGreaterElement.java
@@ -0,0 +1,24 @@
+package Practice_Questions;
+
+public class nextGreaterElement {
+    public static void main(String[] args) {
+
+        // ---Using Array---------
+        // int[] arr = {1,3,2,1,8,6,3,4};
+        // int[] res = new int[arr.length];
+        // for(int i=0; i<arr.length; i++){
+        //     res[i] = -1;
+        //     for(int j=i+1; j<arr.length; j++){
+        //         if(arr[j]>arr[i]){
+        //             res[i] = arr[j];
+        //             break;
+        //         }
+        //     }
+        // }
+        // for(int i=0; i<res.length; i++){
+        //     System.out.print(res[i] + " ");  
+        // }
+
+        // --------------Using Stack---------------------
+    }
+}

5185eefcd4ce153d3b018a10d71305a93bfa2f6c
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-03-31 23:47:05 +0530

    Implementation of Circular queue and deque

---
+++ 08. Queues/deque.java
@@ -0,0 +1,36 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class deque {
+    public static void main(String[] args) {
+        Deque <Integer> dq = new LinkedList<>();
+        // add, remove/poll peek/element
+        // dq.addLast(1);
+        // dq.addLast(2);
+        // dq.addLast(3);
+        // dq.addLast(4);
+        // dq.addLast(5);
+        // System.out.println(dq);
+        // dq.addFirst(5);
+        // System.out.println(dq);
+        // dq.removeLast();
+        // System.out.println(dq);
+        // dq.removeFirst();
+        // System.out.println(dq);
+        // System.out.println(dq.getFirst());
+        // System.out.println(dq.getLast());
+
+
+        dq.addLast(1);
+        dq.addLast(2);
+        dq.addLast(3);
+        dq.addLast(4);
+        dq.add(5);
+        System.out.println(dq);
+        dq.remove();
+        dq.removeAll(dq);
+        System.out.println(dq);
+        dq.removeFirstOccurrence(1);
+        dq.removeFirstOccurrence(3);
+    }
+}

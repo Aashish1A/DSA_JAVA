73e81d4c13cb49baf65bfa84d4d59e9f8860dee7
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-04-01 20:29:25 +0530

    Implementing circularQueue using linked list

--- 08. Queues/CircularQueueImpLL.java
+++ 08. Queues/CircularQueueImpLL.java
@@ -1,5 +1,48 @@
 public class CircularQueueImpLL {
-    public static void main(String[] args) {
+
+    public static class Node{
+        int data;
+        Node next;
+        Node prev;
         
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static class Cql{
+        Node head = null;
+        Node tail = null;
+        int size = 0;
+
+        void add(int val){
+
+        }
+
+        int remove(){
+
+        }
+
+        int peek(){
+
+        }
+
+        int size(){
+
+        }
+
+        boolean isEmpty(){
+            if(size()==0) return false;
+            else return true;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Cql cl = new Cql();
+        cl.add(1);
+        cl.add(2);
+        cl.add(3);
+        cl.add(4);
     }
 }

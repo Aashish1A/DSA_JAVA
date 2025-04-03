c698f65903215e34d94cb46e8fa2072c29b750bc
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-04-03 20:41:55 +0530

    implementing stack using queue

--- 08. Queues/Practice_Questions/stackUsingQueue.java
+++ 08. Queues/Practice_Questions/stackUsingQueue.java
@@ -5,36 +5,45 @@ import java.util.Queue;
 
 public class stackUsingQueue {
 
-    Queue<Integer> q = new LinkedList<>();
+    public static class QueueSt {
+        Queue<Integer> q = new LinkedList<>();
 
-    void push(int val){
-        q.add(val);
-    }
-
-    int pop(){
-        for(int i=1; i<=q.size()-1; i++){
-            q.add(q.remove());
+        void push(int val) {
+            q.add(val);
         }
-        int x = q.remove();
-        return x;
-    }
 
-    int top(){
-        for(int i=1; i<=q.size()-1; i++){
-            q.add(q.remove());
+        int pop() {
+            for (int i = 1; i <= q.size() - 1; i++) {
+                q.add(q.remove());
+            }
+            int x = q.remove();
+            return x;
         }
-        int x = q.peek();
-        q.add(q.remove());
-        return x;
-    }
 
-    boolean empty(){
-        if(q.size()==0) return true;
-        else return false;
-    }
+        int top() {
+            for (int i = 1; i <= q.size() - 1; i++) {
+                q.add(q.remove());
+            }
+            int x = q.peek();
+            q.add(q.remove());
+            return x;
+        }
 
-    
-    public static void main(String[] args) {
+        boolean empty() {
+            if (q.size() == 0)
+                return true;
+            else
+                return false;
+        }
         
+    }
+
+    public static void main(String[] args) {
+        QueueSt st = new QueueSt();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+
     }
 }

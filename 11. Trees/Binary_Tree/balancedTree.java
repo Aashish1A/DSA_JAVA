9e9ba23e1439a8bf09d4cbcc69e05ba18188a558
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-04-16 21:58:18 +0530

    solving problems of binary tree

---
+++ 11. Trees/Binary_Tree/balancedTree.java
@@ -0,0 +1,5 @@
+public class balancedTree {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ 11. Trees/Binary_Tree/diameterOfTree.java
@@ -0,0 +1,5 @@
+public class diameterOfTree {
+    public static void main(String[] args) {
+        
+    }
+}

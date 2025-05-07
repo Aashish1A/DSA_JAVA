d047ee7be65bdff55158f00614ae2eac4d8e9dca
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-05-08 01:05:12 +0530

    Implement bubbleSort

--- 12. Algorithms/Basic.java
+++ 12. Algorithms/Basic.java
@@ -1,6 +1,13 @@
 public class Basic{
     public static void main(String[] args) {
-        int[] arr = new int[5];
-        
+        int[] arr = {6,1,7,3,2,5,4,8,9,9,10};
+        int n = arr.length;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum = sum + arr[i];
+        }
+        int sumOfNum = (n*(n+1))/2;
+        int duplicates = sum - sumOfNum;
+        System.out.println("The duplicates value is : " + duplicates);
     }
 }

4fde2dccd75235c746039a0240c095f37e84d40d
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-06-09 20:47:31 +0530

    implement quick sort

--- 12. Algorithms/quickSort.java
+++ 12. Algorithms/quickSort.java
@@ -1,5 +1,38 @@
-public class quickSort {
+public class QuickSort {
     public static void main(String[] args) {
-        
+        int[] arr = {6, 1, 7, 3, 2, 5, 4, 8, 9, 9, 10};
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        // swap arr[i+1] and arr[high] (pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
     }
 }

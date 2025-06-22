9b9e2f952d102183e7dfeb6d5bc17601d5769507
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-06-22 22:53:48 +0530

    solving problems related backtracking

---
+++ 15. Backtracking/RatMazeOptimized.java
@@ -0,0 +1,38 @@
+public class RatMazeOptimized {
+    private static void print(int sr, int sc, int er, int ec, String s,int[][] maze){
+        if(sr < 0 || sc < 0) return;
+        if(sr > er || sc > ec) return;
+        if(sr==er && sc == ec){
+            System.out.println(s);
+            return;
+        }
+        if(maze[sr][sc]==0) return;
+        if(maze[sr][sc] == -1) return; // this was already visited
+
+        maze[sr][sc] = -1;
+
+        // go rightWays
+        print(sr, sc+1, er, ec, s+"R", maze);
+        // go downWays
+        print(sr+1, sc, er, ec, s+"D", maze);
+        // go left
+        print(sr, sc-1, er, ec, s+"L", maze);
+        // go up
+        print(sr-1, sc, er, ec, s+"U", maze);
+
+        // backtracking
+        maze[sr][sc] = 1;
+    }
+
+    public static void main(String[] args) {
+        int rows = 4;
+        int columns = 6;
+        int[][] maze = {
+            {1,1,0,1,1,1},
+            {1,1,1,1,0,1},
+            {0,1,1,1,1,1},
+            {0,0,1,0,1,1}
+        };
+        print(0,0,rows-1,columns-1, "", maze);
+    }
+}
